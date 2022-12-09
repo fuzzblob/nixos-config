0b15c8fb82fe4f5fcf3d7510b9eff47d58d7c8d2
fuzzblob <admin@fuzzblob.com>
2022-12-09 15:33:15 -0800

    update networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -6,17 +6,49 @@
 
 {
   networking.hostName = "t540p"; # Define your hostname.
-  # Enable networking
-  networking.networkmanager.enable = true;
+  
+  # Networking
+  networking = {
+    networkmanager = {
+      # Enable networking
+      enable = true;
+      ethernet.macAddress = "stable";
+      #insertNameservers = [];
+    
+      # Radios
+      wifi.macAddress = "stable";
+      wifi.powersave = true;
+      wifi.scanRandMacAddress = true;
+    };
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    useDHCP = lib.mkDefault true;
+    # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+    };
+
+    wireless.userControlled.enable = true;
+    wireless.networks = {
+      "free.wifi" = {}; # Public wireless network
+    };
+
+    firewall = {
+      networking.firewall.enable = true;
+      # 631 IPP - printer discovery
+      
+      # Open ports in the firewall.
+      #allowedUDPPorts = [ 631 ];
+      #allowedTCPPorts = [ 631 ];
+    };
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
+  
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -30,11 +62,5 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
 
