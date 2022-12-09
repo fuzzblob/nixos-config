c3757d277d2caed42c190db4f4f4184b744bf1f9
fuzzblob <admin@fuzzblob.com>
2022-12-09 14:27:58 -0800

    move some essentials into systems apps module

--- hosts/t540p/configuration.nix
+++ hosts/t540p/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ../../modules/networking.nix
       ../../modules/locale.nix
+      ../../modules/apps-system.nix
     ];
 
   # Bootloader.
@@ -64,19 +65,11 @@
     description = "user";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox
       vlc
-      git
+      
     #  thunderbird
     ];
   };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
