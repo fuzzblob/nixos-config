2b2fa8079d500c99b5592461fd50fc6c97c47f2f
fuzzblob <admin@fuzzblob.com>
2022-12-09 15:36:08 -0800

    add initial hardening file

---
+++ modules/harden.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+
+  security.sudo.execWheelOnly = true;
+
+  security.auditd.enable = true;
+  security.audit.enable = true;
+
+  services = {
+    openssh = {
+      enable = false;
+      # investigate enabling these for ssh
+      permitRootLogin = "prohibit-password";
+      passwordAuthentication = false;
+      allowSFTP = false;
+    };
+    fail2ban = {
+      enable = true;
+      ignoreIP = [
+      ];
+    };
+  };
+  #nix.settings.allowed-users = [ "root" "srid" ];
+  #nix.settings.trusted-users = [ "root" "srid" ];
+}
